1550596cb189d11e06d097a20dc0487ad31f79cf
mdche001 <mdche@bu.edu>
2019-04-03 07:56:18 +0800

    embed in python

--- function_design.py
+++ function_design.py
@@ -1,8 +1,22 @@
-#Author: mdche@bu.edu
+import matlab.engine
+import os
 
-def analyze_func():
-    """The funcrtion is designed to:
-    1. filter the outliers of the dataset
-    2."""
-    pass
 
+class SolarWindClassifier:
+    def __init__(self):
+        self.dic = os.getcwd()
+
+    def classifier(self, filename):
+        self.file = self.dic + '/' + filename
+        matlab_classifier = matlab.engine.start_matlab('-novm')
+        print("Matlab Engine Loading... ...")
+        classifier_result = matlab_classifier.classify_solar_wind(self.file)
+        print("Predicting... ...")
+        matlab_classifier.quit()
+        prediction = open("./result.dat", "w")
+        prediction.write(classifier_result)
+        prediction.close()
+
+
+S = SolarWindClassifier()
+S.classifier("omni2_2017.dat")
