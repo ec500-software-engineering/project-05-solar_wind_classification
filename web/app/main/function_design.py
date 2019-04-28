493fbb95321649173dde74ef72f386b626f5e271
mdche001 <mdche@bu.edu>
2019-04-28 19:34:19 -0400

    python adjustment for web

--- web/app/main/function_design.py
+++ web/app/main/function_design.py
@@ -1,22 +1,20 @@
 import matlab.engine
 import os
 
-
-class SolarWindClassifier:
-    def __init__(self):
-        self.dic = os.getcwd()
-
-    def classifier(self, filename):
-        self.file = self.dic + '/matlab/' + filename
-        matlab_classifier = matlab.engine.start_matlab('-novm')
-        print("Matlab Engine Loading... ...")
-        classifier_result = matlab_classifier.classify_solar_wind(self.file)
-        print("Predicting... ...")
-        matlab_classifier.quit()
-        prediction = open("./result.dat", "w")
-        prediction.write(classifier_result)
-        prediction.close()
+def classifier():
+    # self.file = self.dic + '/matlab/' + filename
+    matlab_classifier = matlab.engine.start_matlab()
+    matlab_classifier.addpath('./main/matlab')
+    print("Matlab Engine Loading... ...")
+    classifier_result = matlab_classifier.classify_solar_wind(nargout=0)
+    # classifier_result = matlab_classifier.classify_solar_wind(self.file)
+    print("Predicting... ...",type(classifier_result))
+    matlab_classifier.quit()
+    prediction = open("./result.dat", "w")
+    prediction.write(str(classifier_result))
+    prediction.close()
 
 
-S = SolarWindClassifier()
-S.classifier("/matlab/omni2_2017.dat")
+
+classifier()
+
