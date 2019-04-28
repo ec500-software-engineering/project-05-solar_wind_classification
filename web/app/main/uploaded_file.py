8bd2eeac0b6031696fedc304756e638537df522a
mdche001 <mdche@bu.edu>
2019-04-28 18:31:59 -0400

    file arrangement & call model

--- web/app/main/uploaded_file.py
+++ web/app/main/uploaded_file.py
@@ -1,13 +1,21 @@
 import app
+import cv2
+from datetime import timedelta
 from flask import render_template
 from flask import request
 from flask import send_from_directory
+import matlab.engine
 import os
+import time
 from werkzeug.utils import secure_filename
 from . import main
+from . import function_design
 
-UPLOAD_FOLDER = '/Users/y/web/uploads'
+UPLOAD_FOLDER = 'D:/program/ec500_web/app/static/images'
 ALLOWED_EXTENSIONS = set(['png','jpg','jpeg'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.send_file_max_age_default = timedelta(seconds=1)
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -23,7 +31,14 @@ def uploaded_file():
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            user_input = request.form.get("name")
+            upload_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(upload_path)
+
+            img = cv2.imread(upload_path)
+            cv2.imwrite(os.path.join(UPLOAD_FOLDER, 'test.jpg'), img)
+
+            return render_template('upload_ok.html', userinput=user_input, val1=time.time())
             #return training.evaluate_one_image()
             # return vgg1999.accuracy()
             # return 'eee'
